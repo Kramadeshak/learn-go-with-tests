b6875441b3bb56014be2b5a1e4a79ded25c0763d
Sarthak Sharma <sharmasarthak999@gmail.com>
2022-06-18 04:36:06 +0000

    Declared constant. Added empty string functionality to the function Hello

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sarthak")
-	want := "Hello, Sarthak"
+	t.Run("Saying hello to people", func(t *testing.T){
+		got := Hello("Sarthak")
+		want := "Hello, Sarthak"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when empty string is passed", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
