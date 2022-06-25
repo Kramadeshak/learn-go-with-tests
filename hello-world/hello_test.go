9ea6cc06d6ea9fabfb4a526d89cacce36e5d3357
Sarthak Sharma <sharmasarthak999@gmail.com>
2022-06-25 13:11:24 +0000

    Added support for languages. Refactored Tests.

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,20 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("Saying hello to people", func(t *testing.T){
-		got := Hello("Sarthak")
-		want := "Hello, Sarthak"
-
+	assertCorrectMessage := func (t testing.TB, got, want string){
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+	t.Run("Saying hello to people", func(t *testing.T){
+		got := Hello("Sarthak", "")
+		want := "Hello, Sarthak"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when empty string is passed", func(t *testing.T){
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Sarthak", "Spanish")
+		want := "Hola, Sarthak"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T){
+		got := Hello("Sarthak", "French")
+		want := "Bonjour, Sarthak"
+		assertCorrectMessage(t, got, want)
 	})
 }
